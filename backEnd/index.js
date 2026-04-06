6b7f60d6e9fe2e319c23b5ac0033da17e98f0c91
Mazen-mo-10 <mazen.7acebat@gmail.com>
2026-04-06 17:40:29 +0200

    solve error trust

--- backEnd/index.js
+++ backEnd/index.js
@@ -15,6 +15,7 @@ const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 
+app.set("trust proxy", 1);
 // Security Middleware
 app.use(helmet());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
